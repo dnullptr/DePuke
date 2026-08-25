63bd3abeb7cff6cfd14518951cea86e65c96a63f
Ben's Maximus <shitagon@walla.com>
2026-08-25 20:53:10 +0300

    feat: create DePukeListController and DePukeHeaderView for preference pane

--- prefs/DePukeListController.h
+++ prefs/DePukeListController.h
@@ -3,14 +3,8 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 #import <Preferences/PSTableCell.h>
+#import <Preferences/PSHeaderFooterView.h>
 #import <UIKit/UIKit.h>
-
-@protocol PSHeaderFooterView
-- (instancetype)initWithSpecifier:(PSSpecifier *)specifier;
-- (CGFloat)preferredHeightForWidth:(CGFloat)width;
-@optional
-- (CGFloat)preferredHeightForWidth:(CGFloat)width inTableView:(id)tableView;
-@end
 
 @interface DePukeListController : PSListController
 - (void)respring;
